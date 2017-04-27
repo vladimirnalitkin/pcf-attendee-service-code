0a8a6be5fbbf7dfcea6b72f8fb82d646a7bb864b
Vladimir Nalitkin <vladimir.nalitkin@gmail.com>
2017-04-27 17:40:35 -0400

    [test] Add tests

---
+++ ci/tasks/smoke-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+apt-get update && apt-get install -y curl
+
+if [ -z $ATTENDEE_SERVICE_URL ]; then
+  echo "ATTENDEE_SERVICE_URL not set"
+  exit 1
+fi
+
+pushd attendee-service-source
+  echo "Running smoke tests for Attendee Service deployed at $ATTENDEE_SERVICE_URL"
+  smoke-tests/bin/test $ATTENDEE_SERVICE_URL
+popd
+
+exit 0
